02fe2ca19737cc136e060e084015e203c7956873
res-londres <jimuel.londres@gmail.com>
2026-08-22 17:03:31 +0800

    feat: add basic client-server connection using SocketIO

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
 from flask import Flask, render_template
+from flask_socketio import SocketIO
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+
+socketio = SocketIO(app, cors_allowed_origins='*')
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
+@socketio.on('connect')
+def handle_connect():
+    print('[CONNECT] someone connects..')
+
+@socketio.on('disconnect')
+def handle_disconnect():
+    print('[DISCONNECT] someone disconnects..')
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    socketio.run(app, debug=True)
