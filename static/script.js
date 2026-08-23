89cf6549748e866624f218e48fba4196414b0a47
res-londres <jimuel.londres@gmail.com>
2026-08-23 15:23:05 +0800

    feat: add client-side cookie helpers (for setting, deleting cookies)
    - previous commit is server-side cookie helper (for reading cookies)

--- static/script.js
+++ static/script.js
@@ -1,1 +1,18 @@
 const socket = io();
+
+// ---------- cookie-helpers ---------- //
+function setCookie(name, value, days) {
+    const expires = '';
+    if (days) {
+        const date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = '; expires=' + date.toUTCString();
+    }
+    document.cookie = name + '=' + (value || '') + expires + '; path=/; SameSite=Lax';
+    console.log('cookie set: ', name, '=', value);
+    console.log('all cookies: ', document.cookie);
+}
+function deleteCookie(name) {
+    document.cookie = name + '=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+    console.log('cookie deleted:', name);
+}
