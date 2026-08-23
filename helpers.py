5069dcdfb57764e821987bcd37857ed35313e0e3
res-londres <jimuel.londres@gmail.com>
2026-08-23 15:10:48 +0800

    feat: add helper.py; add cookie getter

---
+++ helpers.py
@@ -0,0 +1,8 @@
+from Flask import request
+
+# ---------- cookie helpers ---------- #
+class Cookies:
+    # cookies are set and removed in the client
+    @staticmethod
+    def get_user_cookie():
+        return request.cookies.get('user_id')
