1166e4efe067f2e7d2a2bc7093f1bce29c3f16bf
res-londres <jimuel.londres@gmail.com>
2026-09-03 10:08:56 +0800

    feat: hide and unhide username; copy user id to clipboard

---
+++ static/modules/profile.js
@@ -0,0 +1,30 @@
+
+import { userProfile } from './userProfile.js';
+
+document.getElementById('profile-header').addEventListener('click', function(event) {
+    const actionElement = event.target.closest('[data-action]');
+    if (actionElement) {
+        const action = actionElement.dataset.action;
+        
+        event.stopPropagation();
+        if (action === 'showPrivateUsername') {
+            const privateUsernameElement = document.getElementById('private-username');
+            const showPrivateUsernameButton = document.getElementById('show-private-username-button');
+            const isHidden = privateUsernameElement.classList.toggle('hidden');
+            if (isHidden) {
+                privateUsernameElement.textContent = '[HIDDEN USERNAME]';
+                showPrivateUsernameButton.innerHTML = '<i class="fa-regular fa-eye"></i>';
+            } else {
+                privateUsernameElement.textContent = userProfile.username;
+                showPrivateUsernameButton.innerHTML = '<i class="fa-regular fa-eye-slash"></i>';
+            }
+        } else if (action === 'copyUserID') {
+            const userID = userProfile.user_id;
+            navigator.clipboard.writeText(userID).then(function() {
+                console.log('[PROFILE] user id copied to clipboard: ', userID);
+            }).catch(function(err) {
+                console.error('[PROFILE] could not copy user id: ', err);
+            });
+        }
+    }
+});
--- static/script.js
+++ static/script.js
@@ -3,6 +3,7 @@ import { socket } from './modules/socket.js';
 import * as auth from './modules/auth.js';
 import * as nav from './modules/nav.js';
 import * as post from './modules/post.js';
+import * as profile from './modules/profile.js';
 import * as cookieHelp from './modules/helpers/cookie.js';
 import * as miscHelp from './modules/helpers/misc.js';
 
